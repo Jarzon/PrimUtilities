68074af9d9977249c264097c05946d40d0a13fa8
Jarzon <j@masterj.net>
2017-05-27 17:45:49 -0400

    Adding basic tests

--- tests/projectsTest.php
+++ tests/projectsTest.php
@@ -1,10 +1,44 @@
 <?php
 
+Class ViewMock {
+
+}
+
 class ProjectsTest extends PHPUnit_Framework_TestCase
 {
-    public function testCanReturnFalse()
+    public function testLengthLowerThatMin()
     {
+        $viewMock = new ViewMock();
+        $forms = new \PrimUtilities\Forms($viewMock);
 
-        $this->assertEquals(true, false);
+        $forms->text('', 'test', '', '', 10, 4);
+
+        $post = [
+            'test' => 'a'
+        ];
+
+        try {
+            $forms->verification($post);
+        } catch(\Exception $e) {
+            $this->assertEquals('test is too short', $e->getMessage());
+        }
+    }
+
+    public function testLengthHigherThatMax()
+    {
+        $viewMock = new ViewMock();
+        $forms = new \PrimUtilities\Forms($viewMock);
+
+        $forms->text('', 'test', '', '', 10, 4);
+
+        $post = [
+            'test' => '123456789ab'
+        ];
+
+        try {
+            $forms->verification($post);
+        } catch(\Exception $e) {
+            $this->assertEquals('test is too long', $e->getMessage());
+        }
     }
 }
