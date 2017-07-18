e18d7cdf613bbf772be623b4ee8c213c2c9e8ad8
Jarzon <j@masterj.net>
2017-07-18 10:14:22 -0400

    Fix not being able to pass false as label param

--- src/Forms.php
+++ src/Forms.php
@@ -50,47 +50,47 @@ class Forms
         $this->forms[] = $this->row('password', false, $name, '', $value, $max, 0, '', '');
     }
 
-    public function text($label = '', string $name, string $class = '', string $value = '', $max = false, int $min = 0, string $placeholder = '', string $pattern = '')
+    public function text($label, string $name, string $class = '', string $value = '', $max = false, int $min = 0, string $placeholder = '', string $pattern = '')
     {
         $this->forms[] = $this->row('text', $label, $name, $class, $value, $max, $min, $placeholder, $pattern);
     }
 
-    public function password(string $label = '', string $name, string $class = '', string $value = '', $max = false, int $min = 0, string $placeholder = '')
+    public function password($label, string $name, string $class = '', string $value = '', $max = false, int $min = 0, string $placeholder = '')
     {
         $this->forms[] = $this->row('password', $label, $name, $class, $value, $max, $min, $placeholder, '');
     }
 
-    public function email(string $label = '', string $name, string $class = '', string $value = '', $max = false, int $min = 0, string $placeholder = '')
+    public function email($label, string $name, string $class = '', string $value = '', $max = false, int $min = 0, string $placeholder = '')
     {
         $this->forms[] = $this->row('email', $label, $name, $class, $value, $max, $min, $placeholder, '');
     }
 
-    public function url(string $label = '', string $name, string $class = '', string $value = '', $max = false, int $min = 0, string $placeholder = '')
+    public function url($label, string $name, string $class = '', string $value = '', $max = false, int $min = 0, string $placeholder = '')
     {
         $this->forms[] = $this->row('url', $label, $name, $class, $value, $max, $min, $placeholder, '');
     }
 
-    public function number(string $label = '', string $name, string $class = '', string $value = '', $max = false, $min = false, float $step = 1, string $placeholder = '')
+    public function number($label, string $name, string $class = '', string $value = '', $max = false, $min = false, float $step = 1, string $placeholder = '')
     {
         $this->forms[] = $this->row('number', $label, $name, $class, $value, $max, $min, $placeholder, '', $step);
     }
 
-    public function float(string $label = '', string $name, string $class = '', string $value = '', $max = false, $min = false, float $step = 0.01, string $placeholder = '')
+    public function float($label, string $name, string $class = '', string $value = '', $max = false, $min = false, float $step = 0.01, string $placeholder = '')
     {
         $this->forms[] = $this->row('float', $label, $name, $class, $value, $max, $min, $placeholder, '', $step);
     }
 
-    public function date(string $label = '', string $name, string $class = '', string $value = '', string $placeholder = '', string $pattern = '[0-9]{2}/[0-9]{2}/[0-9]{4}')
+    public function date($label, string $name, string $class = '', string $value = '', string $placeholder = '', string $pattern = '[0-9]{2}/[0-9]{2}/[0-9]{4}')
     {
         $this->forms[] = $this->row('text', $label, $name, $class, $value, false, false, $placeholder, $pattern);
     }
 
-    public function datetime(string $label = '', string $name, string $class = '', string $value = '', string $placeholder = '', string $pattern = '[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2}')
+    public function datetime($label, string $name, string $class = '', string $value = '', string $placeholder = '', string $pattern = '[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2}')
     {
         $this->forms[] = $this->row('text', $label, $name, $class, $value, false, false, $placeholder, $pattern);
     }
 
-    public function select(string $label = '', string $name, string $class = '', array $value = [], string $selected = '')
+    public function select($label, string $name, string $class = '', array $value = [], string $selected = '')
     {
         $this->forms[] = $this->row('select', $label, $name, $class, $value, false, false, '', '', false, $selected);
     }
