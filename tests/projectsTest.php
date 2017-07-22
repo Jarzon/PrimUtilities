b364c96fcb8cb5d0f3f9636e7c1f4bf0db598e37
Jarzon <j@masterj.net>
2017-07-22 15:05:53 -0400

    Test the generated form

--- tests/projectsTest.php
+++ tests/projectsTest.php
@@ -41,4 +41,27 @@ class ProjectsTest extends PHPUnit_Framework_TestCase
             $this->assertEquals('test is too long', $e->getMessage());
         }
     }
+
+    public function testGenerateForms()
+    {
+        $viewMock = new ViewMock();
+        $forms = new \PrimUtilities\Forms($viewMock);
+
+        $forms->text('', 'test', '', '', 10, 4);
+
+        ob_start();
+        $forms->generateForms();
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        // TODO: huh shouldn't echo the content it's inflexible, hard to test and spaces..
+        $this->assertEquals('                                    <label>test                                    <input
+                        type="text"
+                        name="test"
+                        value=""
+                        class=""                                                                                                                                                
+                                                    minlength="4"                            maxlength="10"                                                                            min="4"                            max="10"                        
+                    >
+                                </label>                ', $content);
+    }
 }
